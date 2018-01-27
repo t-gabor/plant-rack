2747d0eaaa07ec39dccb9a4f046074d497b05eb9
tag <tagsoft@users.noreply.github.com>
2018-01-27 14:29:13 +0100

    Add light control.

--- plant-rack.ino
+++ plant-rack.ino
@@ -10,27 +10,32 @@
 #include "wifi-config.h"
 #include "thinger-config.h"
 
-const unsigned long postRateInSeconds = 120;
+#define POST_RATE_IN_SECONDS 120
+#define LIGHT_PIN D3
+#define FAN_PIN D1
+#define SENSOR_PIN D7
 
-DHT dht(D7, DHT22);
+DHT sensor(SENSOR_PIN, DHT22);
 
 ThingerWifi thing(ThingerUsername, ThingerDeviceId, ThingerDeviceCredentials);
+int lightOnHour = 8;
+int lightOffHour = 20;
 
 void setup()
 {
+  pinMode(LIGHT_PIN, OUTPUT);
+  digitalWrite(LIGHT_PIN, HIGH);
+
   Serial.begin(115200);
 
   setupTime();
+  setupThing();
 
-  thing.add_wifi(WiFiSSID, WiFiPSK);
+  pinMode(LIGHT_PIN, OUTPUT);
+  digitalWrite(LIGHT_PIN, HIGH);
 
-  thing["state"] >> [](pson &out) {
-    String t = String(hour(time(nullptr))) + ":" + String(minute(time(nullptr)));
-
-    out["Device Time"] = t;
-    out["Humidity"] = dht.readHumidity();
-    out["Temperature"] = dht.readTemperature();
-  };
+  pinMode(FAN_PIN, OUTPUT);
+  digitalWrite(FAN_PIN, HIGH);
 }
 
 time_t lastSync = 0;
@@ -40,13 +45,18 @@ void loop()
   thing.handle();
 
   time_t now = time(nullptr);
-  if (now > lastSync + postRateInSeconds)
+  if (now > lastSync + POST_RATE_IN_SECONDS)
   {
     if (thing.stream(thing["state"]))
     {
       lastSync = now;
     }
   }
+
+  if (hour(now) >= lightOnHour && hour(now) < lightOffHour)
+    digitalWrite(LIGHT_PIN, LOW);
+  else
+    digitalWrite(LIGHT_PIN, HIGH);
 }
 
 void setupTime()
@@ -61,4 +71,36 @@ void setupTime()
   }
 
   Serial.println(ctime(&now));
+}
+
+void setupThing()
+{
+  thing.add_wifi(WiFiSSID, WiFiPSK);
+
+  thing["state"] >> [](pson &out) {
+    String t = String(hour(time(nullptr))) + ":" + String(minute(time(nullptr)));
+
+    out["Device Time"] = t;
+    out["Humidity"] = sensor.readHumidity();
+    out["Temperature"] = sensor.readTemperature();
+  };
+
+  thing["lightOnHour"] << [](pson &in) {
+    if (in.is_empty())
+      in = lightOnHour;
+    else
+      lightOnHour = in;
+
+    Serial.printf("lightOnHour: %2d\n", lightOnHour);
+
+  };
+
+  thing["lightOffHour"] << [](pson &in) {
+    if (in.is_empty())
+      in = lightOffHour;
+    else
+      lightOffHour = in;
+
+    Serial.printf("lightOffHour: %2d\n", lightOffHour);
+  };
 }
