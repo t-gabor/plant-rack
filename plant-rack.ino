af71442294160d134fa05c7499cb9ca749bf31b3
tag <tagsoft@users.noreply.github.com>
2018-01-27 18:19:04 +0100

    Fan control based on temperature, save values to EEPROM.

--- plant-rack.ino
+++ plant-rack.ino
@@ -4,29 +4,52 @@
 #include <ESP8266WiFi.h>
 #include <ThingerWifi.h>
 #include <DHT.h>
+#include <EEPROM.h>
 
 #include <Time.h>
 
 #include "wifi-config.h"
 #include "thinger-config.h"
 
-#define POST_RATE_IN_SECONDS 120
+#define POST_RATE_IN_SECONDS 60
+
 #define LIGHT_PIN D3
+#define LIGHT_PIN_UNUSED D2
+
 #define FAN_PIN D1
+
 #define SENSOR_PIN D7
 
 DHT sensor(SENSOR_PIN, DHT22);
 
 ThingerWifi thing(ThingerUsername, ThingerDeviceId, ThingerDeviceCredentials);
-int lightOnHour = 8;
-int lightOffHour = 20;
+
+struct Settings
+{
+  int lightOnHour;
+  int lightOffHour;
+  int fanOnTemperature;
+  int fanOffTemperature;
+
+  Settings() : lightOnHour(8),
+               lightOffHour(20),
+               fanOnTemperature(28),
+               fanOffTemperature(25)
+  {
+  }
+};
+
+Settings settings;
+
+float temperature;
+float humidity;
+bool lightOn = false;
+bool fanOn = false;
 
 void setup()
 {
-  pinMode(LIGHT_PIN, OUTPUT);
-  digitalWrite(LIGHT_PIN, HIGH);
-
   Serial.begin(115200);
+  loadSettings();
 
   setupTime();
   setupThing();
@@ -35,7 +58,7 @@ void setup()
   digitalWrite(LIGHT_PIN, HIGH);
 
   pinMode(FAN_PIN, OUTPUT);
-  digitalWrite(FAN_PIN, HIGH);
+  digitalWrite(FAN_PIN, LOW);
 }
 
 time_t lastSync = 0;
@@ -53,14 +76,22 @@ void loop()
     }
   }
 
-  if (hour(now) >= lightOnHour && hour(now) < lightOffHour)
-    digitalWrite(LIGHT_PIN, LOW);
+  if (hour(now) >= settings.lightOnHour && hour(now) < settings.lightOffHour)
+    lightOn = true;
   else
-    digitalWrite(LIGHT_PIN, HIGH);
+    lightOn = false;
+  digitalWrite(LIGHT_PIN, lightOn ? LOW : HIGH); // inverse relay
+
+  if (!fanOn && temperature > settings.fanOnTemperature)
+    fanOn = true;
+  else if (fanOn && temperature < settings.fanOffTemperature)
+    fanOn = false;
+  digitalWrite(FAN_PIN, fanOn ? HIGH : LOW);
 }
 
 void setupTime()
 {
+  Serial.println();
   time_t now = time(nullptr);
   configTime(1 * 3600, 0, "pool.ntp.org", "time.nist.gov");
   while (!now)
@@ -79,28 +110,73 @@ void setupThing()
 
   thing["state"] >> [](pson &out) {
     String t = String(hour(time(nullptr))) + ":" + String(minute(time(nullptr)));
+    humidity = sensor.readHumidity();
+    temperature = sensor.readTemperature();
 
     out["Device Time"] = t;
-    out["Humidity"] = sensor.readHumidity();
-    out["Temperature"] = sensor.readTemperature();
+    out["Humidity"] = humidity;
+    out["Temperature"] = temperature;
+    out["fanOn"] = fanOn;
+    out["lightOn"] = lightOn;
   };
 
   thing["lightOnHour"] << [](pson &in) {
     if (in.is_empty())
-      in = lightOnHour;
+      in = settings.lightOnHour;
     else
-      lightOnHour = in;
+      settings.lightOnHour = in;
 
-    Serial.printf("lightOnHour: %2d\n", lightOnHour);
-
+    Serial.printf("lightOnHour: %2d\n", settings.lightOnHour);
+    saveSettings();
   };
 
   thing["lightOffHour"] << [](pson &in) {
     if (in.is_empty())
-      in = lightOffHour;
+      in = settings.lightOffHour;
     else
-      lightOffHour = in;
+      settings.lightOffHour = in;
 
-    Serial.printf("lightOffHour: %2d\n", lightOffHour);
+    Serial.printf("lightOffHour: %2d\n", settings.lightOffHour);
+    saveSettings();
   };
+
+  thing["fanOnTemperature"] << [](pson &in) {
+    if (in.is_empty())
+      in = settings.fanOnTemperature;
+    else
+      settings.fanOnTemperature = in;
+
+    Serial.printf("fanOnTemperature: %2d\n", settings.fanOnTemperature);
+    saveSettings();
+  };
+
+  thing["fanOffTemperature"] << [](pson &in) {
+    if (in.is_empty())
+      in = settings.fanOffTemperature;
+    else
+      settings.fanOffTemperature = in;
+
+    Serial.printf("fanOffTemperature: %2d\n", settings.fanOffTemperature);
+    saveSettings();
+  };
+}
+
+void loadSettings()
+{
+  EEPROM.begin(128);
+  char flag;
+  EEPROM.get(0, flag);
+  if (flag == 0xb0)
+    EEPROM.get(1, settings);
+  EEPROM.end();
+}
+
+void saveSettings()
+{
+  EEPROM.begin(128);
+  char flag = 0xb0;
+  EEPROM.put(0, flag);
+  EEPROM.put(1, settings);
+  EEPROM.commit();
+  EEPROM.end();
 }
